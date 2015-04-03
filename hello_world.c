5c5755e2dbd6e92f25e5cbba201379aa864afc2b
Breno Carvalho <brenowcarvalho@gmail.com>
2015-04-03 18:53:31 -0300

    Adding another sample algorithm - from another author

--- hello_world.c
+++ hello_world.c
@@ -10,6 +10,24 @@ int main (int argc, char* argv[])
   MPI_Comm_rank (MPI_COMM_WORLD, &rank);        /* get current process id */
   MPI_Comm_size (MPI_COMM_WORLD, &size);        /* get number of processes */
   printf( "Hello world from process %d of %d\n", rank, size );
+	int number;
+	if (rank == 0) {
+		number = 10;
+		MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		//number = -1;
+		MPI_Send(&number, 1, MPI_INT, 2, 1, MPI_COMM_WORLD);
+	} else if (rank == 1) {
+		MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+		         MPI_STATUS_IGNORE);
+		printf("Process 1 received number %d from process 0\n",
+		         number);
+	} else if (rank == 2) {
+		MPI_Recv(&number, 1, MPI_INT, 0, 1, MPI_COMM_WORLD,
+		         MPI_STATUS_IGNORE);
+		printf("Process 2 received number %d from process 0\n",
+		         number);
+	}
+
   MPI_Finalize();
   return 0;
 }
